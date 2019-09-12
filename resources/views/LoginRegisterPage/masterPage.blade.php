da1ce656ca8d8366fdcb0be5eeb74f2d748dd9f1
asip <asiphamdi@gmail.com>
2019-09-12 11:18:49 +0700

    initial

---
+++ resources/views/LoginRegisterPage/masterPage.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chikuso Store</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="auth/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="auth/css/style.css">
+</head>
+<body>
+
+    <div class="">
+
+        <!-- Sign up form -->
+
+        <!-- Sing in  Form -->
+
+    </div>
+
+    <!-- JS -->
+    <script src="auth/vendor/jquery/jquery.min.js"></script>
+    <script src="auth/js/main.js"></script>
+</body>
+</html>
