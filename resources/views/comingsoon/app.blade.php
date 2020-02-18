771ac3ca328c7f1853c59ad1550276b242cd5cc1
AsipHamdi <asiphamdi13@gmail.com>
2020-02-18 12:08:22 +0700

    update

--- resources/views/comingsoon/app.blade.php
+++ resources/views/comingsoon/app.blade.php
@@ -5,10 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-	<title>Chikuso Store</title>
 	<link rel="icon" href="{{ asset('icon/shopping-cart.png') }}" type="image/x-icon">
-
+	<title>Chikuso Store</title>
+	
 	{{-- CSS --}}
 	<link rel="stylesheet" type="text/css" href="comingsoon/css/util.css">
 	<link rel="stylesheet" type="text/css" href="comingsoon/css/main.css">
