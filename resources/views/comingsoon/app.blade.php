133b6e1ecbab91c0e5c86829e4f22ef310459a7e
AsipHamdi <asiphamdi13@gmail.com>
2020-02-18 12:11:20 +0700

    update

--- resources/views/comingsoon/app.blade.php
+++ resources/views/comingsoon/app.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	
 	<link rel="icon" href="{{ asset('icon/shopping-cart.png') }}" type="image/x-icon">
 	<title>Chikuso Store</title>
 	
